ba7938624cd4fabaadcef160ceaf5f65f11cd2e2
th3akii <160267784+th3akii@users.noreply.github.com>
2025-11-23 20:06:53 +0100

    dev (#1)
    * Popravka tezinski btn dizajn
    
    * Srednjen tezinski btn skroz
    
    * Popravljen pogresan input
    
    * Opcija za brisanje cvora
    
    * dodata opcija da se vidi da li je usmeren
    
    * obrisi granu
    
    * Srednjeno dodavanje duplih grana
    
    * Refactor graph edge addition logic into a dedicated method
    
    * radi importovanje yupi
    
    * Fix missing newline at end of file in Home.razor

--- Grafovi/Models/Graf.cs
+++ Grafovi/Models/Graf.cs
@@ -12,5 +12,50 @@ namespace Grafovi.Models
             cvorovi = new List<GrafCvor>();
             grane = new List<GrafGrana>();
         }
+
+        public void ObrisiCvor(GrafCvor cvor)
+        {
+            grane.RemoveAll(g => g.pocetniCvor == cvor || g.krajnjiCvor == cvor);
+            cvorovi.Remove(cvor);
+        }
+        
+        public void ObrisiGranu(GrafGrana grana)
+        {
+            cvorovi.RemoveAll(c => c == grana.pocetniCvor || c == grana.krajnjiCvor);
+            grane.Remove(grana);
+        }
+
+        public void Dodaj(string pocetniNaziv, string krajnjiNaziv, double tezinaInput, bool usmerenGraf)
+        {
+            var pocetniCvor = cvorovi.FirstOrDefault(c => c.naziv == pocetniNaziv);
+            if (pocetniCvor == null)
+            {
+                pocetniCvor = new GrafCvor(cvorovi.Count + 1, pocetniNaziv);
+                cvorovi.Add(pocetniCvor);
+            }
+
+            var krajnjiCvor = cvorovi.FirstOrDefault(c => c.naziv == krajnjiNaziv);
+            if (krajnjiCvor == null)
+            {
+                krajnjiCvor = new GrafCvor(cvorovi.Count + 1, krajnjiNaziv);
+                cvorovi.Add(krajnjiCvor);
+            }
+
+            var granaPostoji = grane.Any(g =>
+                (g.pocetniCvor == pocetniCvor && g.krajnjiCvor == krajnjiCvor) ||
+                (!usmerenGraf && g.pocetniCvor == krajnjiCvor && g.krajnjiCvor == pocetniCvor)
+            );
+            if (!granaPostoji)
+            {
+                grane.Add(new GrafGrana
+                {
+                    ID = grane.Count + 1,
+                    pocetniCvor = pocetniCvor,
+                    krajnjiCvor = krajnjiCvor,
+                    tezina = tezinaInput,
+                    usmerena = false
+                });
+            }
+        }
     }
 }

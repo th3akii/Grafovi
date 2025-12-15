b8b1b422fee6f28c33223d5373da6205fb9bdc39
th3akii <160267784+th3akii@users.noreply.github.com>
2025-12-15 19:29:40 +0100

    kruskal v1 (#1)

---
+++ Grafovi/Models/Algoritmi/GrafAlgoritamKruskal.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using System.Linq;
+using Grafovi.Models;
+using Grafovi.Models.Obrada;
+
+namespace Grafovi.Models.Algoritmi
+{
+    public class GrafAlgoritamKruskal
+    {
+        public List<GrafGrana> KruskalAlgoritam(Graf graf)
+        {
+            List<GrafGrana> sveGrane = new List<GrafGrana>(graf.grane);
+            sveGrane.Sort((a, b) => a.tezina.CompareTo(b.tezina));
+
+            UnionFind unionFind = new UnionFind(graf);
+            List<GrafGrana> graneZaBrisanje = new List<GrafGrana>();
+
+            foreach (var grana in sveGrane)
+            {
+                int korenCvor1 = unionFind.Nadji(grana.pocetniCvor.ID);
+                int korenCvor2 = unionFind.Nadji(grana.krajnjiCvor.ID);
+
+                if (korenCvor1 != korenCvor2)
+                {
+                    unionFind.Spoji(korenCvor1, korenCvor2);
+                }
+                else
+                {
+                    graneZaBrisanje.Add(grana);
+                }
+            }
+
+            return graneZaBrisanje;
+        }
+    }
+
+    class UnionFind
+    {
+        private Dictionary<int, int> roditelj;
+        private Dictionary<int, int> rank;
+
+        public UnionFind(Graf graf)
+        {
+            roditelj = new Dictionary<int, int>();
+            rank = new Dictionary<int, int>();
+
+            foreach (var cvor in graf.cvorovi)
+            {
+                roditelj[cvor.ID] = cvor.ID;
+                rank[cvor.ID] = 0;
+            }
+        }
+
+        public int Nadji(int id)
+        {
+            if (roditelj[id] != id)
+            {
+                roditelj[id] = Nadji(roditelj[id]);
+            }
+
+            return roditelj[id];
+        }
+
+        public void Spoji(int i, int j)
+        {
+            int korenI = Nadji(i);
+            int korenJ = Nadji(j);
+
+            if (korenI == korenJ)
+            {
+                return;
+            }
+
+            if (rank[korenI] < rank[korenJ])
+            {
+                roditelj[korenI] = korenJ;
+            }
+            else if (rank[korenI] > rank[korenJ])
+            {
+                roditelj[korenJ] = korenI;
+            }
+            else
+            {
+                roditelj[korenJ] = korenI;
+                rank[korenI]++;
+            }
+        }
+    }
+}
